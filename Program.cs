eb25566f7f25df7b56c6ad685b962a35bc410ec4
Fernando <fernando.d.g.leite@gmail.com>
2024-11-06 17:30:34 -0300

    tudo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Iphone iphone = new Iphone("500", "Iphone 15 plus", "300", 200);
+Nokia nokia = new Nokia("3234", "Nokia Tijolao", "300", 500);
+string nomeApp = "Whatsapp";
+iphone.InstalarAplicativo(nomeApp);
+nokia.InstalarAplicativo(nomeApp);
